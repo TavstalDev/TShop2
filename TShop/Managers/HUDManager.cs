d459d29dd786662426f4d6467a5d7a8473ba354a
Tavstal <tavstal@gmail.com>
2023-08-13 18:04:49 +0200

    Renamed UIManager to HUDManager

---
+++ TShop/Managers/HUDManager.cs
@@ -0,0 +1,31 @@
+﻿using Rocket.API;
+using Rocket.Core.Plugins;
+using System;
+using Rocket.API.Collections;
+using UnityEngine;
+using Rocket.Unturned.Chat;
+using SDG.Unturned;
+using System.Linq;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Events;
+using Rocket.Unturned;
+using System.Collections.Generic;
+using System.IO;
+using Math = System.Math;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Tavstal.TShop.Compability;
+using Tavstal.TShop.Managers;
+using Logger = Tavstal.TShop.Helpers.LoggerHelper;
+using Tavstal.TShop.Compability.Hooks;
+using Tavstal.TLibrary.Compatibility;
+using Tavstal.TLibrary.Extensions;
+
+namespace Tavstal.TShop.Managers
+{
+    public class HUDManager
+    {
+       
+
+    }
+}
